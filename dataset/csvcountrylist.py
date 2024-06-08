7a341e3e81590b47387e9f9e50ece93d24138059
rjz954 <162874975+rjz954@users.noreply.github.com>
2024-06-08 16:36:06 +0200

    Add files via upload

--- dataset/csvcountrylist.py
+++ dataset/csvcountrylist.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 # Read the merged CSV file into a DataFrame
 merged_df = pd.read_csv('merged_data.csv')
+
 # Drop duplicates based on 'T' column, keeping the first instance
-filtered_df = merged_df.drop_duplicates(subset='T', keep='first')
+filtered_df = merged_df.drop_duplicates(subset='Region/Country/Area', keep='first')
+
 # Initialize a list to store the values
 values = []
+
 # Iterate through the filtered DataFrame
 for index, row in filtered_df.iterrows():
     if pd.notna(row[1]):
@@ -20,6 +23,9 @@ for index, row in filtered_df.iterrows():
                 break
         if not value_found:
             values.append(None)
+
 # Create a DataFrame with 'T' and the extracted values
-result_df = pd.DataFrame({'T': filtered_df['T'], 'Value': values})
+result_df = pd.DataFrame({'Region/Country/Area': filtered_df['Region/Country/Area'], 'Value': values})
+
+# Save the result to a new CSV file
 result_df.to_csv('Country_values.csv', index=False)
