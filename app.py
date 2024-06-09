ccdc02aa167b54e5d47783297df5584054ff5870
rjz954 <162874975+rjz954@users.noreply.github.com>
2024-06-09 13:06:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,45 +4,47 @@ import re
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 
+
 app = Flask(__name__)
 
-# Determine the directory of the CSV file
+
 base_dir = os.path.dirname(os.path.abspath(__file__))
 csv_file_path = os.path.join(base_dir, 'dataset', 'Country_values.csv')
 
-# Load country IDs and names from the CSV file into a dictionary
+
 country_name_map = {}
 with open(csv_file_path, mode='r', newline='', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
-        # Filter out unwanted characters using regular expressions
         country_name = re.sub(r'[^a-zA-Z()\[\] ]', '', row['value'])
         country_name_map[country_name] = int(row['id'])
 
-# Load unique series from the CSV file and sort alphabetically
+
 unique_series = []
 with open(os.path.join(base_dir, 'dataset', 'Unique_series.csv'), mode='r', newline='', encoding='utf-8') as file:
     reader = csv.reader(file)
-    next(reader)  # Skip header
+    next(reader)
     unique_series = sorted([row[0] for row in reader])
+
 
 @app.route('/')
 def index():
     return redirect(url_for('search'))
+
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
         country_name = request.form['country_name']
         series = request.form['series']
-        search_type = request.form.get('search_type', 'info')  # Default to 'info' if search_type is not provided
-        # Get the ID corresponding to the entered country name
+        search_type = request.form.get('search_type', 'info')
         country_id = country_name_map.get(country_name)
         if search_type == 'value':
             return search_by_value(country_id, series)
         else:
             return search_by_info(country_id, series)
     return render_template('search_form.html', countries=sorted(country_name_map.keys()), unique_series=unique_series)
+
 
 def search_by_value(country_id, series):
     conn = psycopg2.connect(dbname="countrydata", user="country_user", password="password", host="localhost", port="5432")
@@ -59,6 +61,7 @@ def search_by_value(country_id, series):
     conn.close()
     return render_template('search_results_value.html', countries=countries)
 
+
 def search_by_info(country_id, series):
     conn = psycopg2.connect(dbname="countrydata", user="country_user", password="password", host="localhost", port="5432")
     cur = conn.cursor()
@@ -73,6 +76,7 @@ def search_by_info(country_id, series):
     countries = cur.fetchall()
     conn.close()
     return render_template('search_results_info.html', countries=countries)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
